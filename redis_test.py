49a0e5973036358b0fbc7cecfef3c61aeac2fd8e
HongruiZhao <hongrui.zhao@yahoo.com>
2025-01-09 11:00:54 -0800

    improve redis test

--- redis_test.py
+++ redis_test.py
@@ -1,12 +1,12 @@
 import redis
 import pickle
 import numpy as np
-
+import datetime
 # Redis connection
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=False)
 
 theta_i = np.zeros(1633360).astype(np.float32)
-uncertainty_i  = np.zeros(1628176).astype(np.float32)
+uncertainty_i  = np.zeros(1633360).astype(np.float32)
 
 
 while True:
@@ -20,8 +20,9 @@ while True:
     # get data
     agent_j = redis_client.get('agent_j')
     if agent_j:
+        print(f"{datetime.datetime.now()}: receive agent j!")
         agent_j = pickle.loads(agent_j)
         array_1 = agent_j['theta_j']
         array_2 = agent_j['uncertainty_j']
 
-        print(f'theta_j = {array_1}, uncertainty_j= {array_2}')
+        print(f'theta_j = {array_1.shape}, uncertainty_j= {array_2.shape}')
